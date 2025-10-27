4ed06b4f5e7417be4f8f7ff384353ed85f1b5c6b
Andrey Lebedev <andreylebedev345@gmail.com>
2025-10-26 23:49:37 -0700

    Refactor CloseIcon and NoAgentNotification components to utilize new WarningIcon and improve prop handling

--- frontend/components/CloseIcon.tsx
+++ frontend/components/CloseIcon.tsx
@@ -1,6 +1,15 @@
-export function CloseIcon() {
+import type { SVGProps } from "react";
+
+export function CloseIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
       <path
         d="M3.33398 3.33334L12.6673 12.6667M12.6673 3.33334L3.33398 12.6667"
         stroke="currentColor"
